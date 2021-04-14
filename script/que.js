58bd37320622f2c4430890df596e3a7a9781b1a8
youxfans <412386861@qq.com>
2021-04-14 14:09:33 +0800

    que.js

--- script/que.js
+++ script/que.js
@@ -25,46 +25,52 @@ const rightText = ["任何瞬间的心动都不容易，不要怠慢了它——
 
 const wrongText = ["错误提示语言1", "错误提示语言2"];
 
-$(function () {
-  function init() {
-    const q = data[Math.floor(Math.random() * data.length)];
-
-    var ansResult = $(".as").first().id === q.answer;
-
-    function getText(arr) {
-      return arr[Math.floor(arr.length * Math.random())];
-    }
-
-    $("#queBox").html(`
-      <div class="qt">${q.question}
-        <img src="${q.image}" class="qti">
+// 逻辑
+let q;
+function init() {
+  console.log("init");
+  q = data[Math.floor(Math.random() * data.length)];
+  $("#queBox").html(`
+    <div class="qt">${q.question}
+      <img src="${q.image}" class="qti">
+    </div>
+    <div class="ass">
+      <div class="a1" id="a1" data-id="A">
+        <img src="${q.options.A.image}" class="a1i">
+        <div>A</div>
+        <div>${q.options.A.text}</div>
       </div>
-      <div class="ass">
-        <div class="a1" id="a1">
-          <img src="${q.options.A.image}" class="a1i">
-          <div>A</div>
-          <div>${q.options.A.text}</div>
-        </div>
-        <div class="a1" id="a2">
-          <img src="${q.options.B.image}" class="a1i">
-          <div>B</div>
-          <div>${q.options.B.text}</div>
-        </div>
-        <div class="a1" id="a3">
-          <img src="${q.options.C.image}" class="a1i">
-          <div>C</div>
-          <div>${q.options.C.text}</div>
-        </div>
+      <div class="a1" id="a2" data-id="B">
+        <img src="${q.options.B.image}" class="a1i">
+        <div>B</div>
+        <div>${q.options.B.text}</div>
       </div>
-    `);
+      <div class="a1" id="a3" data-id="C">
+        <img src="${q.options.C.image}" class="a1i">
+        <div>C</div>
+        <div>${q.options.C.text}</div>
+      </div>
+    </div>
+  `);
+}
 
-    if (ansResult) {
-      $("#ansText").html(getText(rightText));
-    } else {
-      $("#ansText").html(getText(wrongText));
-    }
+function sub() {
+  if (!$(".as").first()) return;
+  console.log($(".as").first());
+  var ansResult = $(".as").first().attr("data-id") === q.answer;
+
+  function getText(arr) {
+    return arr[Math.floor(arr.length * Math.random())];
   }
 
+  if (ansResult) {
+    $("#ansText").html(getText(rightText));
+  } else {
+    $("#ansText").html(getText(wrongText));
+  }
+}
+
+$(function () {
   init();
 
   // todo
